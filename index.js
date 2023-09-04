4f29918a49cad0f07070910c1ad1c91f1a9117c2
Serhat Esen <serhatesen39@gmail.com>
2023-09-04 18:55:23 +0300

    test

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import ejs from 'ejs';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+// ToDo listesini saklamak için boş bir dizi oluşturun
+const todoList = [];
+
+// Ana sayfa için route
+app.get('/', (req, res) => {
+  res.render('todo', { todoList });
+});
+
+// Yeni görev eklemek için POST route
+app.post('/addTodo', (req, res) => {
+  const newTodo = {
+    task: req.body.task,
+    status: 'Devam Ediyor...',
+  };
+  todoList.push(newTodo);
+  res.redirect('/');
+});
+
+// Görevi silmek için POST route
+app.post('/deleteTodo', (req, res) => {
+  const index = req.body.index;
+  if (index >= 0 && index < todoList.length) {
+    todoList.splice(index, 1);
+  }
+  res.redirect('/');
+});
+
+// Görevi tamamlamak için POST route
+app.post('/finishTodo', (req, res) => {
+  const index = req.body.index;
+  if (index >= 0 && index < todoList.length) {
+    todoList[index].status = 'Tamamlandı';
+  }
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`ToDo uygulaması ${port} portunda çalışıyor.`);
+});
