741e20880d456adbf9f119508630dc6c21b9ae99
Serhat Esen <serhatesen39@gmail.com>
2023-11-02 17:10:33 +0300

    test

--- index.js
+++ index.js
@@ -1,52 +1,48 @@
-import express from 'express';
-import ejs from 'ejs';
-import bodyParser from 'body-parser';
+import express from "express";
+import ejs from "ejs";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.engine('ejs', ejs.renderFile);
-app.set('view engine', 'ejs');
+app.engine("ejs", ejs.renderFile);
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
 
 // ToDo listesini saklamak için boş bir dizi oluşturun
 const todoList = [];
 
 // Ana sayfa için route
-app.get('/', (req, res) => {
-  res.render('todo', { todoList });
+app.get("/", (req, res) => {
+  res.render("todo", { todoList });
 });
 
 // Yeni görev eklemek için POST route
-app.post('/addTodo', (req, res) => {
+app.post("/addTodo", (req, res) => {
   const newTodo = {
     task: req.body.task,
-    status: 'Devam Ediyor...',
+    status: "Devam Ediyor...",
   };
   todoList.push(newTodo);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // Görevi silmek için POST route
-app.post('/deleteTodo', (req, res) => {
+app.post("/deleteTodo", (req, res) => {
   const index = req.body.index;
   if (index >= 0 && index < todoList.length) {
     todoList.splice(index, 1);
   }
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // Görevi tamamlamak için POST route
-app.post('/finishTodo', (req, res) => {
+app.post("/finishTodo", (req, res) => {
   const index = req.body.index;
   if (index >= 0 && index < todoList.length) {
-    todoList[index].status = 'Tamamlandı';
+    todoList[index].status = "Tamamlandı";
   }
-  res.redirect('/');
+  res.redirect("/");
 });
 
 app.listen(port, () => {
